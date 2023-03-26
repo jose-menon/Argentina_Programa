40f204ac1c7705ee50cb933840a4fea3cb61fb84
jose-menon <123137667+jose-menon@users.noreply.github.com>
2023-03-26 13:03:44 -0300

    Add files via upload

--- Funciones.js
+++ Funciones.js
@@ -1,16 +1,27 @@
-let Bot_Datos = document.getElementById('Bot_Datos');
-let Datos = document.getElementById('Datos');
+let Bot_Datos = document.getElementById("Bot_Datos");
+let Regreso = document.getElementById("Regreso");
+let Bot_Contacto = document.getElementById("Bot_Contacto");
 
-document.getElementById('Bot_Datos').addEventListener('click' , Mostrar());
+Bot_Datos.addEventListener("click" , Mostrar);
+Regreso.addEventListener("click" , Ocultar);
+Bot_Contacto.addEventListener("click" , Mostrar);
 
 function Mostrar() {
-    Datos.classList.toggle('show');
-
-    if (Datos.classList.contains('show') ) {
-        Datos.innerHTML = 'Volver';
-    }
-    else {
-        Datos.innerHTML = " ";
-    }
+    document.getElementById("Datos").style.display = "block";
+    document.getElementById("Regreso").style.display = "block";
 }
 
+function Ocultar() {
+    document.getElementById("Datos").style.display = "none";
+    document.getElementById("Regreso").style.display = "none";
+}
+
+function Mostrar() {
+    document.getElementById("Datos_Contac").style.display = "block";
+    document.getElementById("Regreso").style.display = "block";
+}
+
+function Ocultar() {
+    document.getElementById("Datos_Contac").style.display = "none";
+    document.getElementById("Regreso").style.display = "none";
+}
